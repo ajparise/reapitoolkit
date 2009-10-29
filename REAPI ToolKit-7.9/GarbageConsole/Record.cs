bdf3d806f7305eb89e9cd831e60b384927963b5e
ajparise_cp <SND\ajparise_cp>
2009-10-29 19:51:11 +0000

    Clean up

---
+++ REAPI ToolKit-7.9/GarbageConsole/Record.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GarbageConsole
+{
+    public class Record
+    {
+        [RaisersEdge.API.ToolKit.Managed.Mapping.Attributes.RecordMapping(Blackbaud.PIA.RE7.BBREAPI.ERECORDSFields.RECORDS_fld_ID, true)]
+        public int ID { get; set; }
+
+        [RaisersEdge.API.ToolKit.Managed.Mapping.Attributes.RecordMapping(Blackbaud.PIA.RE7.BBREAPI.ERECORDSFields.RECORDS_fld_CONSTITUENT_ID, true)]
+        public string ConstituentID { get; set; }
+
+        [RaisersEdge.API.ToolKit.Managed.Mapping.Attributes.RecordMapping(Blackbaud.PIA.RE7.BBREAPI.ERECORDSFields.RECORDS_fld_FIRST_NAME)]
+        public string FirstName { get; set; }
+
+        [RaisersEdge.API.ToolKit.Managed.Mapping.Attributes.RecordMapping(Blackbaud.PIA.RE7.BBREAPI.ERECORDSFields.RECORDS_fld_LAST_NAME)]
+        public string LastName { get; set; }
+
+        [RaisersEdge.API.ToolKit.Managed.Mapping.Attributes.RecordMapping(Blackbaud.PIA.RE7.BBREAPI.ERECORDSFields.RECORDS_fld_MARITAL_STATUS)]
+        public string MartialStatus { get; set; }
+    }
+}
